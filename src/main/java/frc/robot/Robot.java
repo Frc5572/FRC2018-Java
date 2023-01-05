caf315752426383b2ac830ee20597b047564ff77
Alex Resnick <adr8292@gmail.com>
2023-01-04 22:15:39 -0600

    finish linting fixes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,9 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+/**
+ * Robot file.
+ */
 public class Robot extends TimedRobot {
     private RobotContainer robotContainer;
 
